ac5ec908280592691a97e4adfca3c12d5d413530
CoderMusgrove <codermusgrove@gmail.com>
2014-03-30 12:44:27 -0500

    Update GrowControl.java

--- src/me/codermusgrove/growcontrol/GrowControl.java
+++ src/me/codermusgrove/growcontrol/GrowControl.java
@@ -1,18 +1,26 @@
 package me.codermusgrove.growcontrol;
 
+import java.util.Arrays;
+
 import org.bukkit.Material;
+import org.bukkit.TreeType;
 import org.bukkit.block.Block;
-import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockGrowEvent;
+import org.bukkit.event.world.StructureGrowEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class GrowControl extends JavaPlugin implements Listener {
 
-	private boolean wheat, potato, carrot, netherwart, melon, pumpkin, vine, sugarcane;
-	private String[] list = { "wheat-grow", "potato-grow", "carrot-grow", "netherwart-grow", "melon-grow", "pumpkin-grow", "vine-grow",
-			"sugarcane-grow" };
+	private boolean grow, wheat, potato, carrot, netherwart, melon, pumpkin, vine, sugarcane, trees, mushrooms, treesbm, mushroomsbm;
+	private String[] list = { "grow", "wheat-grow", "potato-grow", "carrot-grow", "netherwart-grow", "melon-grow", "pumpkin-grow", "vine-grow",
+			"sugarcane-grow", "tree-grow", "mushroom-grow", "tree-bonemeal", "mushroom-bonemeal" };
+	private TreeType[] treetype = { TreeType.TREE, TreeType.BIG_TREE, TreeType.ACACIA, TreeType.BIRCH, TreeType.DARK_OAK, TreeType.JUNGLE,
+			TreeType.JUNGLE_BUSH, TreeType.MEGA_REDWOOD, TreeType.REDWOOD, TreeType.SMALL_JUNGLE, TreeType.SWAMP, TreeType.TALL_BIRCH,
+			TreeType.TALL_REDWOOD };
+	private TreeType[] mushroomType = { TreeType.BROWN_MUSHROOM, TreeType.RED_MUSHROOM };
 
 	@Override
 	public void onEnable() {
@@ -22,25 +30,37 @@ public class GrowControl extends JavaPlugin implements Listener {
 	}
 
 	private void loadValues() {
-		FileConfiguration config = getConfig();
-		wheat = config.getBoolean("wheat-grow", true);
-		potato = config.getBoolean("potato-grow", true);
-		carrot = config.getBoolean("carrot-grow", true);
-		netherwart = config.getBoolean("netherwart-grow", true);
-		melon = config.getBoolean("melon-grow", true);
-		pumpkin = config.getBoolean("pumpkin-grow", true);
-		vine = config.getBoolean("vine-grow", true);
-		sugarcane = config.getBoolean("sugarcane-grow", true);
-
 		for (int i = 0; i < list.length; i++) {
-			if (config.getString(list[i]) == null) config.set(list[i], true);
+			if (getConfig().getString(list[i]) == null) {
+				getConfig().set(list[i], true);
+				continue;
+			}
+			if (getConfig().getString(list[i]).equals("")) {
+				getConfig().set(list[i], true);
+				continue;
+			}
 		}
+		saveConfig();
+		grow = getConfig().getBoolean(list[0]);
+		wheat = getConfig().getBoolean(list[1]);
+		potato = getConfig().getBoolean(list[2]);
+		carrot = getConfig().getBoolean(list[3]);
+		netherwart = getConfig().getBoolean(list[4]);
+		melon = getConfig().getBoolean(list[5]);
+		pumpkin = getConfig().getBoolean(list[6]);
+		vine = getConfig().getBoolean(list[7]);
+		sugarcane = getConfig().getBoolean(list[8]);
+		trees = getConfig().getBoolean(list[9]);
+		mushrooms = getConfig().getBoolean(list[10]);
+		treesbm = getConfig().getBoolean(list[11]);
+		mushroomsbm = getConfig().getBoolean(list[12]);
 	}
 
 	@EventHandler
 	public void onBlockGrow(BlockGrowEvent e) {
 		Block b = e.getBlock();
 		Material t = b.getType();
+		if (!grow) return;
 		if (t == Material.CROPS && !wheat) e.setCancelled(true);
 		if (t == Material.POTATO && !potato) e.setCancelled(true);
 		if (t == Material.CARROT && !carrot) e.setCancelled(true);
@@ -49,5 +69,19 @@ public class GrowControl extends JavaPlugin implements Listener {
 		if ((t == Material.PUMPKIN_STEM || t == Material.PUMPKIN) && !pumpkin) e.setCancelled(true);
 		if (t == Material.VINE && !vine) e.setCancelled(true);
 		if (t == Material.SUGAR_CANE_BLOCK && !sugarcane) e.setCancelled(true);
+	}
+
+	@EventHandler
+	public void onStructureGrow(StructureGrowEvent e) {
+		TreeType t = e.getSpecies();
+		Player p = e.getPlayer();
+		if (Arrays.asList(treetype).contains(t) && !trees) {
+			if (p == null) e.setCancelled(true);
+			else if (p != null && !treesbm) e.setCancelled(true);
+		}
+		if (Arrays.asList(mushroomType).contains(t) && !mushrooms) {
+			if (p == null) e.setCancelled(true);
+			else if (p != null && !mushroomsbm) e.setCancelled(true);
+		}
 	}
 }
